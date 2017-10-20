d5b9f174b92cec2ce902f30fcc6e22f18e3fb07b
Rick Butterfield <rick.butterfield@gmail.com>
2017-10-20 14:41:01 +0100

    Tidy up

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,11 +18,15 @@ $(document).ready(function() {
   });
 
   $(document).on('click', 'button', function(e) {
-    $(this)
+    var self = this;
+    $(self)
       .closest('.response-buttons')
-      .slideUp();
-    $('#input').val(this.innerText);
-    send();
+      .fadeUp(200);
+
+    setTimeout(function() {
+      $('#input').val(self.innerText);
+      send();
+    }, 500);
   });
 });
 
@@ -32,10 +36,17 @@ function send() {
     $('.chat').prepend(
       '<div class="response response-user"><p>' + text + '</p></div>'
     );
-    $('.response-bot')
-      .last()
-      .find('.response-buttons')
-      .slideUp();
+    if (
+      $('.response-bot')
+        .first()
+        .find('.response-buttons')
+        .css('display') === 'block'
+    ) {
+      $('.response-bot')
+        .first()
+        .find('.response-buttons')
+        .fadeUp();
+    }
     $('#input').val('');
     $.ajax({
       type: 'POST',
@@ -61,40 +72,100 @@ function send() {
   }
 }
 function setResponse(val) {
-  var json = JSON.parse(val);
-  console.log(json);
+  if (val.indexOf('{') === 0) {
+    var json = JSON.parse(val);
 
-  if (json.result.fulfillment.speech) {
-    var html = '';
-    html += '<div class="response response-bot">';
-    html +=
-      '<div class="flag flag--top">' +
-      '<div class="flag__image">' +
-      '<img src="assets/img/logo.png" width="24">' +
-      '</div>' +
-      '<div class="flag__body">' +
-      '<p>' +
-      json.result.fulfillment.speech +
-      '</p>' +
-      '</div>' +
-      '</div>';
+    if (json.result.fulfillment.speech) {
+      var html = '';
+      html += '<div class="response response-bot">';
+      html +=
+        '<div class="flag flag--bottom">' +
+        '<div class="flag__image">' +
+        '<img src="assets/img/logo.png" width="24">' +
+        '</div>' +
+        '<div class="flag__body">' +
+        '<p>' +
+        json.result.fulfillment.speech +
+        '</p>' +
+        '</div>' +
+        '</div>';
 
-    for (var i = 0; i < json.result.fulfillment.messages.length; i++) {
-      var item = json.result.fulfillment.messages[i];
-      if (item.platform === 'facebook') {
-        var replies = item.replies;
-        if (replies) {
-          html += '<div class="response-buttons">';
-          for (var j = 0; j < replies.length; j++) {
-            html += '<button>' + replies[j] + '</button>';
+      for (var i = 0; i < json.result.fulfillment.messages.length; i++) {
+        var item = json.result.fulfillment.messages[i];
+        if (item.platform === 'facebook') {
+          var replies = item.replies;
+          if (replies) {
+            html += '<div class="response-buttons">';
+            for (var j = 0; j < replies.length; j++) {
+              html += '<button>' + replies[j] + '</button>';
+            }
+
+            html += '</div>';
           }
-
-          html += '</div>';
         }
       }
-    }
 
-    html += '</div>';
-    $('.chat').prepend(html);
+      html += '</div>';
+      setTimeout(function() {
+        $('.chat').prepend(html);
+      }, 500);
+    }
   }
 }
+
+var getAnimOpts = function(a, b, c) {
+  if (!a) {
+    return {
+      duration: 'normal'
+    };
+  }
+  if (!!c) {
+    return {
+      duration: a,
+      easing: b,
+      complete: c
+    };
+  }
+  if (!!b) {
+    return {
+      duration: a,
+      complete: b
+    };
+  }
+  if (typeof a === 'object') {
+    return a;
+  }
+  return {
+    duration: a
+  };
+};
+
+var getUnqueuedOpts = function(opts) {
+  return {
+    queue: false,
+    duration: opts.duration,
+    easing: opts.easing
+  };
+};
+
+//Add fadeDown animation
+$.fn.fadeDown = function(a, b, c) {
+  var slideOpts = getAnimOpts(a, b, c),
+    fadeOpts = getUnqueuedOpts(slideOpts);
+  $(this)
+    .hide()
+    .css('opacity', 0)
+    .slideDown(slideOpts)
+    .animate({ opacity: 1 }, fadeOpts);
+};
+
+//Add fadeUp animation
+$.fn.fadeUp = function(a, b, c) {
+  var slideOpts = getAnimOpts(a, b, c),
+    fadeOpts = getUnqueuedOpts(slideOpts);
+  $(this)
+    .show()
+    .css('opacity', 1)
+    .slideUp(slideOpts)
+    .animate({ opacity: 0 }, fadeOpts);
+};
