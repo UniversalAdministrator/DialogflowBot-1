f9e8106f4be272ddec1b094b9c6786595e84807f
Rick Butterfield <rick.butterfield@gmail.com>
2017-10-20 11:47:37 +0100

    Add enter key code

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var accessToken = 'bad6bc0b4d4f430ca10e48e91c9391d8';
 var baseUrl = 'https://api.api.ai/v1/';
 $(document).ready(function() {
   $('#input').keypress(function(event) {
-    if (event.which == 13) {
+    if (event.which == 13 || event.keyCode == 13) {
       event.preventDefault();
       send();
     }
